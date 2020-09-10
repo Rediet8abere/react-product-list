201e0bd77d60186992816816f5fbfb995513685c
Rediet8abere <redietaberegesesse@gmail.com>
2020-09-09 18:31:51 -0700

    button compoenent

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import data, { categoriesUnique } from './data'
 import './App.css';
 
 categoriesUnique.push("All")
-console.log(categoriesUnique)
+// console.log(categoriesUnique)
+
+function CatagoryButton(props) {
+  return (
+      <button className={props.catButton === props.currCat ? "button-header-active"  : "button-header"}
+        key={props.catButton} onClick={props.onClick}>
+          {props.catButton}
+      </button>
+  )
+  
+}
 
 function Products(props) {
-  console.log("props: ", props)
   return (
-    
+    // make a component 
     <div key={props.product.id} className="products-header">
           <h1 className="block">{props.product.name}</h1>
           <h4 className="block">{props.product.price}</h4>
@@ -26,17 +35,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          {categoriesUnique.map((category) => {             
-              return <button className={category === currentCategory ? "button-header-active"  : "button-header" }
-              // className="button-header" 
-              key={category} onClick={() => setCategory(category)}>
-                {category}
-                </button>
+          {categoriesUnique.map((category) => {
+              return <CatagoryButton catButton={category} currCat={currentCategory} onClick={() => setCategory(category)}/>
             })}
    
 
-          {data.filter((item) => { 
-             console.log(currentCategory)    
+          {data.filter((item) => {    
              if (currentCategory === "All") {
               return item
              } else {
